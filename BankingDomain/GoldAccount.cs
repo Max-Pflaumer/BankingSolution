357c5dbb481f65ef96f7b4b9c3f4d8e428a6c484
Max Pflaumer <42851053+Max-Pflaumer@users.noreply.github.com>
2020-09-15 19:10:35 +0000

    Done with this

---
+++ BankingDomain/GoldAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingDomain
+{
+    public class GoldAccount : BankAccount //Gold account inherits bank account
+    {
+        public override void Deposit(decimal amountToDeposit)
+        {
+            base.Deposit(amountToDeposit * 1.10M);
+        }
+    }
+}
---
+++ BankingUnitTests/GoldAccountDeposits.cs
@@ -0,0 +1,24 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingUnitTests
+{
+    public class GoldAccountDeposits
+    {
+        [Fact]
+        public void GoldAccountGetBonus()
+        {
+            var account = new GoldAccount();//Gold Deposit is the exact same as normal besides deposit
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+
+            account.Deposit(amountToDeposit);
+
+            Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
+
+        }
+    }
+}
